fcb42ed4c782af660ae3bb830116537139ddbe98
VictorRocha <rocharv95@gmail.com>
2025-11-08 08:36:57 -0300

    update project

--- index.php
+++ index.php
@@ -2,43 +2,40 @@
 
     <main id="main-content">
         <p>Bem-vindo ao sistema de registro financeiro da Rocha Developments</p>
-        <h2>Novo Registro</h2>
+        <h2>Registro</h2>
         <div class="container text-center">
-            <div class="row">
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-                <div class="col">
-                    
-                </div>
-            </div>
+            <form method="GET" action="">
+                <table class="table table-dark table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Valor</th>
+                            <th scope="col">Descrição</th>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Forma Pgto</th>
+                            <th scope="col">Banco</th>
+                            <th scope="col">Recorrente</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($registros as $registro): ?>
+                            <tr>
+                                <td><?php echo $registro['id']; ?></td>
+                                <td><?php echo $registro['data']; ?></td>
+                                <td><?php echo $registro['valor']; ?></td>
+                                <td><?php echo $registro['descricao']; ?></td>
+                                <td><?php echo $registro['tipo']; ?></td>
+                                <td><?php echo $registro['categoria']; ?></td>
+                                <td><?php echo $registro['forma_pgto']; ?></td>
+                                <td><?php echo $registro['banco']; ?></td>
+                                <td><?php echo $registro['recorrencia']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </form>
         </div>
 
     </main>
